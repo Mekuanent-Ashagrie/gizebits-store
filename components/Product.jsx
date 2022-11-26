e6e3168a9b9234eb3044f48f24b2c93056001996
mekuanent-ashagrie <maquintosh20@gmail.coom>
2022-11-26 15:33:49 +0300

    last chanage

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
 
-const Product = () => {
-  return (
-    <div>Product</div>
+const Product = (props) => {  
+  const product = props.product;
+  return (    
+    <div>
+      <Link href={`/product/${product.slug.current}`}>
+        <div className='product-card'>
+          <img src={urlFor(product.image && product.image[0])} 
+          width={250}
+          height={250}
+          className="product-image"
+          alt=''/>
+          <p className='product-name'>{product.name}</p>
+          <p className='product-price'>{product.price} ብር</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
